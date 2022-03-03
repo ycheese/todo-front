8d7010d98d1057b0d97f3430a994c0a565993914
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-03-03 22:19:18 +0900

    material ui 실습

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React from "react";
 import Todo from './Todo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todo/>
-      <Todo/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [
+        {id:0, title:"Hello World 1", done:true},
+        {id:1, title:"Hello World 2", done:false},
+      ],
+    };
+  }
+
+  render(){
+    var todoItems = this.state.items.map((item, idx) => (
+      <Todo item={item} key={item.id}/>
+    ));
+
+    return <div className="App">{todoItems}</div>;
+  }
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,30 @@
-function Todo() {
-    return (
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
-            <label for="todo0">Todo 컴포넌트 만들기</label>
-        </div>
-    );
+import React from 'react';
+import {ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core";
+
+class Todo extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {item:props.item};
+    }
+    render(){
+        const item = this.state.item;
+        return (
+            <ListItem>
+                <Checkbox checked={item.done}/>
+                <ListItemText>
+                    <InputBase
+                        inputProps={{"aria-label" : "naked"}}
+                        type="text"
+                        id={item.id}
+                        name={item.id}
+                        value={item.title}
+                        multiline={true}
+                        fullWidth={true}
+                    />
+                </ListItemText>
+            </ListItem>
+        );
+    }
 }
 
 export default Todo;
