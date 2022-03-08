1a492952d22dad799aafab63fe0ea48a0244baa3
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-03-08 23:35:55 +0900

    Todo 수정 이벤트 추가

--- src/Todo.js
+++ src/Todo.js
@@ -5,12 +5,31 @@ import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
 class Todo extends React.Component {
     constructor(props){
         super(props);
-        this.state = {item:props.item};
+        this.state = {item:props.item, readOnly:true};
         this.delete = props.delete;
     }
 
     deleteEventHandler = () => {
         this.delete(this.state.item);
+    }
+
+    offReadOnlyMode = () => {
+        console.log("Event : " + this.state.readOnly);
+        this.setState({readOnly:false}, () => {
+            console.log("ReadOnly ==> ", this.state.readOnly);
+        });
+    }
+
+    enterKeyEventHandler = (e) => {
+        if(e.key === "Enter"){
+            this.setState({readOnly:true});
+        }
+    }
+
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({item:thisItem});
     }
 
     render(){
@@ -20,13 +39,19 @@ class Todo extends React.Component {
                 <Checkbox checked={item.done} disableRipple/>
                 <ListItemText>
                     <InputBase
-                        inputProps={{"aria-label" : "naked"}}
+                        inputProps={{
+                            "aria-label" : "naked",
+                            readOnly : this.state.readOnly,
+                        }}
                         type="text"
                         id={item.id}
                         name={item.id}
                         value={item.title}
                         multiline={true}
                         fullWidth={true}
+                        onClick={this.offReadOnlyMode}
+                        onChange={this.editEventHandler}
+                        onKeyPress={this.enterKeyEventHandler}
                     />
                 </ListItemText>
                 <ListItemSecondaryAction>
