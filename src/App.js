b14de8a502c81b8a41a3d863091cea8f6889e6b3
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-03-13 13:18:04 +0900

    Todo 아이템 조회 API 호출 추가

--- src/App.js
+++ src/App.js
@@ -12,6 +12,28 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    const requestOptions = {
+      method : "GET",
+      headers : {"Content-Type" : "application/json"},
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        this.setState({
+          items : response.data,
+        });
+      },
+      (error) => {
+        this.setState({
+          error,
+        });
+      }
+    );
+  }
+
   add = (item) => {
     const thisItems = this.state.items;
     item.id = "ID-"+thisItems.length;
