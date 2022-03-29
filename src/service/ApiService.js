b301486e9be0e6bf62d1b54b68747dc6c3481f44
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-03-29 20:41:52 +0900

    로컬 스토리지 액세스 토큰 저장 로직 추가

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,13 +1,23 @@
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request){
+    let headers = new Headers({
+        "Content-Type" : "application/json",
+    });
+
+    // 로컬 스토리지에서 ACCESS_TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken && accessToken !== null){
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     let options = {
-        headers : new Headers({
-            "Content-Type" : "application/json",
-        }),
+        headers : headers,
         url : API_BASE_URL + api,
         method : method,
     };
+
     if(request){
         // GET method
         options.body = JSON.stringify(request);
@@ -22,10 +32,10 @@ export function call(api, method, request){
         })
     )
     .catch((error) => {
-        //console.log(error);
-        //if(error.status === 403){
+        console.log(error.status);
+        if(error.status === 403){
             window.location.href = "/login";
-        //}
+        }
 
         return Promise.reject(error);
     });
@@ -34,7 +44,11 @@ export function call(api, method, request){
 export function signin(userDTO){
     return call("/auth/signin", "POST", userDTO)
     .then((response) => {
-        console.log("response : ", response);
-        alert("로그인 토큰 : " + response.token);
+        if(response.token){
+            // 로컬 스토리지에 토큰 저장
+            localStorage.setItem("ACCESS_TOKEN", response.token);
+            // token 이 존재하는 경우 Todo 화면으로 redirect
+            window.location.href = "/";
+        }
     });
 }
